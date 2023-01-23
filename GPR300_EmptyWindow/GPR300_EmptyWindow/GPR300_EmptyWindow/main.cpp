0243f76a108f91f999b9f18ff3cf5e2ab08805ac
NeoKattan <neokatttan@gmail.com>
2023-01-23 16:18:17 -0500

    1/23/23 classwork color changing triangle

--- GPR300_EmptyWindow/GPR300_EmptyWindow/GPR300_EmptyWindow/main.cpp
+++ GPR300_EmptyWindow/GPR300_EmptyWindow/GPR300_EmptyWindow/main.cpp
@@ -7,13 +7,33 @@
 void resizeFrameBufferCallback(GLFWwindow* window, int width, int height);
 
 //TODO: Vertex shader source code
-//const char* vertexShaderSource = "...";
+const char* vertexShaderSource =
+"#version 450							\n"
+"layout (location = 0) in vec3 vPos;	\n"
+"layout (location = 1) in vec4 vColor;	\n"
+"out vec4 Color;						\n"
+"void main(){							\n"
+"   Color = vColor;						\n"
+"   gl_Position = vec4(vPos,1.0);		\n"
+"}										\0";
 
 //TODO: Fragment shader source code
-//const char* fragmentShaderSource = "...";
+const char* fragmentShaderSource =
+"#version 450									\n"
+"out vec4 FragColor;							\n"
+"in vec4 Color;									\n"
+"uniform float _Time;							\n"
+"void main(){									\n"
+"   FragColor = vec4(abs(sin(_Time))*Color);	\n"
+"}												\0";
 
 //TODO: Vertex data array
-//const float vertexData[] = { ... };
+const float vertices[] = {
+	//	x		y		z		color (rgba)
+		-0.5,	-0.5,	0.0,	1.0, 0.0, 0.0, 1.0,
+		0.5,	-0.5,	0.0,	0.0, 1.0, 0.0, 1.0,
+		0.0,	0.5,	0.0,	0.0, 0.0, 1.0, 1.0,
+};
 
 int main() {
 	if (!glfwInit()) {
@@ -33,61 +53,103 @@ int main() {
 
 
 	//TODO: Create and compile vertex shader
-	//glCreateShader(...)
-	//glShaderSource(...)
-	//glCompileShader(...)
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
 
 	//TODO: Get vertex shader compilation status and output info log
-	//glGetShaderiv(...)
-	//glGetShaderInfoLog(...)
+	//Get result of last compile - either GL_TRUE or GL_FALSE
+	GLint success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//Dump logs into a char array - 512 is an arbitrary length
+		GLchar infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile vertex shader: %s", infoLog);
+	}
 	
 	//TODO: Create and compile fragment shader
-	//glCreateShader(...)
-	//glShaderSource(...)
-	//glCompileShader(...)
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
 
 	//TODO: Get fragment shader compilation status and output info log
-	//glGetShaderiv(...)
-	//glGetShaderInfoLog(...)
+	//GLint success;
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//Dump logs into a char array - 512 is an arbitrary length
+		GLchar infoLog[512];
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		printf("Failed to compile fragment shader: %s", infoLog);
+	}
 
 	//TODO: Create shader program
-	//glCreateProgram()
+	GLuint shaderProgram = glCreateProgram();
 
 	//TODO: Attach vertex and fragment shaders to shader program
-	//glAttachShader(...)
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
 
 	//TODO: Link shader program
-	//glLinkProgram(...)
+	glLinkProgram(shaderProgram);
 
 	//TODO: Check for link status and output errors
-	//glGetProgramiv(...)
-	//glGetProgramInfoLog(...)
+	//GLint success;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		GLchar infoLog[512];
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("Failed to link shader program %s", infoLog);
+	}
 
 	//TODO: Delete vertex + fragment shader objects
-	//glDeleteShader(...)
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	//TODO: Create and bind Vertex Array Object (VAO)
-	//glGenVertexArrays(...)
-	//glBindVertexArray(...)
+
+	//Create a new vertex array object
+	unsigned int vertexArrayObject;
+	glGenVertexArrays(1, &vertexArrayObject);
+
+	//Bind this new VAO so that subsequent function calls will modify it
+	glBindVertexArray(vertexArrayObject);
 
 	//TODO: Create and bind Vertex Buffer Object (VBO), fill with vertexData
-	//glGenBuffers(...)
-	//glBindBuffer(...)
-	//glBufferData(...)
+
+	//Generate a buffer with an id.
+	unsigned int vertexBufferObject;
+	glGenBuffers(1, &vertexBufferObject);
+
+	//Bind the new buffer. GL_ARRAY_BUFFER is the binding target for vertex attributes.
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+
+	//Create a new data store.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	//TODO: Define vertex attribute layout
-	//glVertexAttribPointer(...)
-	//glEnableVertexAttribArray(...)
+
+	//POSITION
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	//COLOR
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.2f, 0.3f, 0.6f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//TODO:Use shader program
-		//glUseProgram(...)
+		glUseProgram(shaderProgram);
+
+		//Set uniforms
+		float time = (float)glfwGetTime();
+		glUniform1f(glGetUniformLocation(shaderProgram, "_Time"), time);
 		
 		//TODO: Draw triangle (3 indices!)
-		//glDrawArrays(...)
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
